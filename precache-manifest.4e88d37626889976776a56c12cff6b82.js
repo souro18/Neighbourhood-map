13a5afb5da666306c8de6bf20277fa63df550a46
souro18 <sourajit.paul61@gmail.com>
2020-05-23 10:39:24 +0530

    Updates

---
+++ precache-manifest.4e88d37626889976776a56c12cff6b82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "255ee021ce8e61edccf0",
+    "url": "/Neighbourhood-map/static/js/runtime~main.255ee021.js"
+  },
+  {
+    "revision": "b077c0e37d1b594aba28",
+    "url": "/Neighbourhood-map/static/js/main.b077c0e3.chunk.js"
+  },
+  {
+    "revision": "ac9ca8b002f0277f2f9f",
+    "url": "/Neighbourhood-map/static/js/1.ac9ca8b0.chunk.js"
+  },
+  {
+    "revision": "b077c0e37d1b594aba28",
+    "url": "/Neighbourhood-map/static/css/main.a1fdfed1.chunk.css"
+  },
+  {
+    "revision": "55faf94dcb31110b0d196c99cf78bd05",
+    "url": "/Neighbourhood-map/index.html"
+  }
+];
---
+++ serviceWorker.js
@@ -0,0 +1,27 @@
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open('mysite-dynamic').then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        if(response) { console.log("from cache");}
+        return response || fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        }).catch(function() {
+          console.log("Fetch Error. No internet.");
+        });
+      });
+    })
+  );
+});
+const urlsToCache =[
+      '/',
+      '/index.html'];
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open('mysite-dynamic')
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
