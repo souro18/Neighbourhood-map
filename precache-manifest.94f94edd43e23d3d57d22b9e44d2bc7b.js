ec59f5329c25bd68b82a21eb1d7809fc9202cbea
souro18 <sourajit.paul61@gmail.com>
2020-06-29 22:25:21 +0530

    Updates

--- precache-manifest.94f94edd43e23d3d57d22b9e44d2bc7b.js
+++ precache-manifest.94f94edd43e23d3d57d22b9e44d2bc7b.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/Neighbourhood-map/static/js/runtime~main.255ee021.js"
   },
   {
-    "revision": "98a1032f7b954daa0246",
-    "url": "/Neighbourhood-map/static/js/main.98a1032f.chunk.js"
+    "revision": "6a1e5c239d256f8701ab",
+    "url": "/Neighbourhood-map/static/js/main.6a1e5c23.chunk.js"
   },
   {
     "revision": "ac9ca8b002f0277f2f9f",
     "url": "/Neighbourhood-map/static/js/1.ac9ca8b0.chunk.js"
   },
   {
-    "revision": "98a1032f7b954daa0246",
-    "url": "/Neighbourhood-map/static/css/main.a1fdfed1.chunk.css"
+    "revision": "6a1e5c239d256f8701ab",
+    "url": "/Neighbourhood-map/static/css/main.cfdc56ed.chunk.css"
   },
   {
-    "revision": "ef738844f22a090992631e5319c3a3d8",
+    "revision": "a15e2a0173eea7bf46e7394708d173cc",
     "url": "/Neighbourhood-map/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Neighbourhood-map/precache-manifest.b1cf81be012f5bda8a2578a89246fa6b.js"
+  "/Neighbourhood-map/precache-manifest.94f94edd43e23d3d57d22b9e44d2bc7b.js"
 );
 
 workbox.clientsClaim();
